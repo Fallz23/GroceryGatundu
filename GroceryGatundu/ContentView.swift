4449aa2a0350dbba8e00fc154e672b27643d9888
fall23 <joshua.gatundu27@d155.org>
2026-02-04 07:59:59 -0600

    Initial Commit

---
+++ GroceryGatundu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceryGatundu
+//
+//  Created by JOSHUA GATUNDU on 2/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceryGatundu/GroceryGatunduApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryGatunduApp.swift
+//  GroceryGatundu
+//
+//  Created by JOSHUA GATUNDU on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct GroceryGatunduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
